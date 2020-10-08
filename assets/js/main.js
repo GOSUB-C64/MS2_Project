d5be12b298865182e9c42f624fef97a4633cb266
Chris Curran <chriscurran8@yahoo.com>
2020-10-08 22:30:57 +0000

    saving work

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,8 @@ $(window).resize(function () {
   responsiveGrid();
 });
 
-let tileId, tileColor, tileIdString;
-// let tileSeq = [];
+let tileIdString;
 let answerSeq = []; // holds users guess for comparison later
-// let tileColorSeq = []; // store colours of tiles
 var gameCount = 3; /* game starts at 1 grid square(tile) toggling on/off then increments by 1 each time the user is successful */
 var isClickEnabled = false;
 var noOfClicks = 0;
@@ -58,21 +56,27 @@ function getColour(arr) {
     "IndianRed",
   ];
   let index = 0;
-  let tileColorSeq = [];
-  while (tileColorSeq.length < 16) {
+  let colArr = [];
+  while (colArr.length < 16) {
     nextColour = arr[index];
 
-    tileColorSeq.push(colourMap[nextColour]);
+    colArr.push(colourMap[nextColour]);
     index++;
   }
-  console.log(tileColorSeq);
+  console.log(colArr);
+  return colArr;
 }
-// display on screen and save current tile to 'currentTile'
-// function displayColouredTile(nextTile, colour) {
-//   var tileId = "#tile" + nextTile; // build element ID
-//   $(tileId).css("background-color", colour);
-//   return;
-// }
+
+
+function displayColouredTile(tileArr, colArr) {
+  
+  let Id = "#tile" + tileArr[index];
+  $(Id).css("background-color", colArr[index]);
+  
+  index++;
+  
+  return Id;
+}
 
 // function displayCurrentSeq(tileSeq, x) {
 //   isClickEnabled = false;
@@ -99,83 +103,83 @@ function getColour(arr) {
 
 function blinkTile() {
   tileSeq = generateTileArray();
-  getColour(tileSeq);
-  //   tileColorSeq.push(tileColor);
-  //   displayColouredTile(tileId, tileColor);
-  //   var intervalID = setInterval(() => {
-  //     $("#tile" + tileId).css("background-color", "#000");
-  //     if (tileSeq.length < gameCount) {
-  //       blinkTile();
-  //     } else {
-  //       acceptUserInput();
-  //     }
-  //     clearTimeout(intervalID);
-  //   }, 2000);
+  tileColourSeq = getColour(tileSeq);
+  
+  let tileId = displayColouredTile(tileSeq, tileColourSeq);
+    let intervalID = setInterval(() => {
+      $("#tile" + tileId).css("background-color", "#000");
+      if (tileSeq.length < gameCount) {
+        blinkTile();
+      } else {
+        acceptUserInput();
+      }
+      clearTimeout(intervalID);
+    }, 2000);
 }
 
-// function acceptUserInput() {
-//   isClickEnabled = true;
-// }
+function acceptUserInput() {
+  isClickEnabled = true;
+}
 
 // ////////// Main Game Logic //////////
 
 blinkTile();
 // console.log(tileSeq);
 
-// console.log("clicks(outside) = ", noOfClicks);
-// if (noOfClicks === gameCount) {
-//   console.log("FINAL TEST");
-//   // Seq same
-//   if (JSON.stringify(tileSeq) === JSON.stringify(answerSeq)) {
-//     alert("WIn");
-//   }
-// }
+console.log("clicks(outside) = ", noOfClicks);
+if (noOfClicks === gameCount) {
+  console.log("FINAL TEST");
+  // Seq same
+  if (JSON.stringify(tileSeq) === JSON.stringify(answerSeq)) {
+    alert("WIn");
+  }
+}
 
-// $(".tile").click(function () {
-//   if (!isClickEnabled) return;
-//   //   $(tileSeq[gameCount-1]).css("background-color", "#000");
+$(".tile").click(function () {
+  if (!isClickEnabled) return;
+  //   $(tileSeq[gameCount-1]).css("background-color", "#000");
 
-//   noOfClicks++;
-//   if (noOfClicks <= gameCount) {
-//     console.log("clicks = ", noOfClicks);
+  noOfClicks++;
+  if (noOfClicks <= gameCount) {
+    console.log("clicks = ", noOfClicks);
 
-//     tileIdString = $(this).attr("id"); // build the ID of which of the 16 elements (divs) was clicked
-//     tileId = parseInt(tileIdString.split("tile")[1]);
-//     console.log(tileId);
-//     answerSeq.push(tileId);
+    tileIdString = $(this).attr("id"); // build the ID of which of the 16 elements (divs) was clicked
+    tileId = parseInt(tileIdString.split("tile")[1]);
+    console.log(tileId);
+    answerSeq.push(tileId);
 
-//     // Glow the tile
-//     var tileColor = getColour(tileId);
-//     displayColouredTile(tileId, tileColor); // display users guess to screen grid
+    // Glow the tile
+    var tileColor = getColour(tileId);
+    displayColouredTile(tileId, tileColor); // display users guess to screen grid
 
-//     // (un)-glow the tile
-//     var intervalID = setInterval(() => {
-//       $("#tile" + tileId).css("background-color", "#000");
-//       debugger;
-//       clearTimeout(intervalID);
-//     }, 1000);
-//   }
-//   console.log("NUMBER OF CLICKS = ", noOfClicks);
+    // (un)-glow the tile
+    var intervalID = setInterval(() => {
+      $("#tile" + tileId).css("background-color", "#000");
+    
+      clearTimeout(intervalID);
+    }, 1000);
+  }
+  console.log("NUMBER OF CLICKS = ", noOfClicks);
 
-//   if (tileId !== tileSeq[index]) {
-//     console.log(tileId, tileSeq[index]);
-//     alert("! GAME OVER !");
-//     debugger;
-//   } else if (noOfClicks === gameCount) {
-//     console.log("comparing noOfClicks to gameCount!!!");
-//     //   $(".tile").off("click");
-//     gameCount++; // increment game level (add 1 more tile)
-//     noOfClicks = 0; // reset the clicks
+  if (tileId !== tileSeq[index]) {
+    console.log(tileId, tileSeq[index]);
+    alert("! GAME OVER !");
 
-//     answerSeq = []; // clear out the users' answer array
-//     index = 0;
+  } else if (noOfClicks === gameCount) {
+    console.log("comparing noOfClicks to gameCount!!!");
+    //   $(".tile").off("click");
+    gameCount++; // increment game level (add 1 more tile)
+    noOfClicks = 0; // reset the clicks
 
-//     tileId = pickTile(); // get a number between 1/16 inc
-//     tileColor = getColour(tileId);
-//     tileColorSeq.push(tileColor);
-//     let x = 0;
-//     displayCurrentSeq(tileSeq, x);
-//   } else {
-//     index++;
-//   }
-// });
+    answerSeq = []; // clear out the users' answer array
+    index = 0;
+
+    tileId = pickTile(); // get a number between 1/16 inc
+    tileColor = getColour(tileId);
+    tileColorSeq.push(tileColor);
+    let x = 0;
+    displayCurrentSeq(tileSeq, x);
+  } else {
+    index++;
+  }
+});
