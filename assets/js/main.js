8ec85d19a0b07e246b9f93dbd401ee90d5a13566
Chris Curran <chriscurran8@yahoo.com>
2020-10-08 18:55:24 +0000

    assigned colours to array by passing another array to function

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,9 @@ $(window).resize(function () {
 });
 
 let tileId, tileColor, tileIdString;
-
+// let tileSeq = [];
 let answerSeq = []; // holds users guess for comparison later
-let tileColorSeq = []; // store colours of tiles
+// let tileColorSeq = []; // store colours of tiles
 var gameCount = 3; /* game starts at 1 grid square(tile) toggling on/off then increments by 1 each time the user is successful */
 var isClickEnabled = false;
 var noOfClicks = 0;
@@ -25,7 +25,6 @@ var index = 0;
 // generate array 16 non-repeatable random nos (1-16) representing the 4x4 grid
 function generateTileArray() {
   let tileSeq = [];
-
   while (tileSeq.length < 16) {
     let tile = Math.floor(Math.random() * 16) + 1;
 
@@ -34,147 +33,149 @@ function generateTileArray() {
     }
   }
   console.log(tileSeq);
+  return tileSeq;
 }
 
 // assign each tile (div) its own color
-function getColour([]) {
-  let nextColour;
-  var colourMap = {
-    1: "Red",
-    2: "Green",
-    3: "Blue",
-    4: "Yellow",
-    5: "Orange",
-    6: "Magenta",
-    7: "White",
-    8: "Violet",
-    9: "Cyan",
-    10: "Brown",
-    11: "Grey",
-    12: "Teal",
-    13: "DeepPink",
-    14: "GreenYellow",
-    15: "GoldenRod",
-    16: "IndianRed",
-  };
-  let x = 0;
+function getColour(arr) {
+  var nextColour;
+  var colourMap = [
+    "Red",
+    "Green",
+    "Blue",
+    "Yellow",
+    "Orange",
+    "Magenta",
+    "White",
+    "Violet",
+    "Cyan",
+    "Brown",
+    "Grey",
+    "Teal",
+    "DeepPink",
+    "GreenYellow",
+    "GoldenRod",
+    "IndianRed",
+  ];
+  let index = 0;
+  let tileColorSeq = [];
   while (tileColorSeq.length < 16) {
-    nextColour = tileSeq[x];
+    nextColour = arr[index];
+
     tileColorSeq.push(colourMap[nextColour]);
-    x++;
+    index++;
   }
   console.log(tileColorSeq);
 }
 // display on screen and save current tile to 'currentTile'
-function displayColouredTile(nextTile, colour) {
-  var tileId = "#tile" + nextTile; // build element ID
-  $(tileId).css("background-color", colour);
-  return;
-}
+// function displayColouredTile(nextTile, colour) {
+//   var tileId = "#tile" + nextTile; // build element ID
+//   $(tileId).css("background-color", colour);
+//   return;
+// }
 
-function displayCurrentSeq(tileSeq, x) {
-  isClickEnabled = false;
-  console.log("IN displayCurrentSeq fn");
+// function displayCurrentSeq(tileSeq, x) {
+//   isClickEnabled = false;
+//   console.log("IN displayCurrentSeq fn");
 
-  tileId = tileSeq[x];
-  tileColor = tileColorSeq[x];
-  displayColouredTile(tileId, tileColor); // this line works
-  var intervalID = setInterval(() => {
-    $("#tile" + tileId).css("background-color", "#000");
-    if (x < tileSeq.length) {
-      console.log("IN if statement of displayCurrentSeq fn");
+//   tileId = tileSeq[x];
+//   tileColor = tileColorSeq[x];
+//   displayColouredTile(tileId, tileColor); // this line works
+//   var intervalID = setInterval(() => {
+//     $("#tile" + tileId).css("background-color", "#000");
+//     if (x < tileSeq.length) {
+//       console.log("IN if statement of displayCurrentSeq fn");
 
-      x++;
-      displayCurrentSeq(tileSeq, x);
-    } else {
-      acceptUserInput();
-    }
-    clearTimeout(intervalID);
-    console.log("TIMEOUT CLEARED!!");
-  }, 1000);
-  return;
-}
-
+//       x++;
+//       displayCurrentSeq(tileSeq, x);
+//     } else {
+//       acceptUserInput();
+//     }
+//     clearTimeout(intervalID);
+//     console.log("TIMEOUT CLEARED!!");
+//   }, 1000);
+//   return;
+// }
 
 function blinkTile() {
-  generateTileArray();
-  tileColor = getColour(tileSeq);
-  tileColorSeq.push(tileColor);
-  displayColouredTile(tileId, tileColor);
-  var intervalID = setInterval(() => {
-    $("#tile" + tileId).css("background-color", "#000");
-    if (tileSeq.length < gameCount) {
-      blinkTile();
-    } else {
-      acceptUserInput();
-    }
-    clearTimeout(intervalID);
-  }, 2000);
+  tileSeq = generateTileArray();
+  getColour(tileSeq);
+  //   tileColorSeq.push(tileColor);
+  //   displayColouredTile(tileId, tileColor);
+  //   var intervalID = setInterval(() => {
+  //     $("#tile" + tileId).css("background-color", "#000");
+  //     if (tileSeq.length < gameCount) {
+  //       blinkTile();
+  //     } else {
+  //       acceptUserInput();
+  //     }
+  //     clearTimeout(intervalID);
+  //   }, 2000);
 }
 
-function acceptUserInput() {
-  isClickEnabled = true;
-}
+// function acceptUserInput() {
+//   isClickEnabled = true;
+// }
 
-////////// Main Game Logic //////////
+// ////////// Main Game Logic //////////
 
 blinkTile();
-console.log(tileSeq);
+// console.log(tileSeq);
 
-console.log("clicks(outside) = ", noOfClicks);
-if (noOfClicks === gameCount) {
-  console.log("FINAL TEST");
-  // Seq same
-  if (JSON.stringify(tileSeq) === JSON.stringify(answerSeq)) {
-    alert("WIn");
-  }
-}
+// console.log("clicks(outside) = ", noOfClicks);
+// if (noOfClicks === gameCount) {
+//   console.log("FINAL TEST");
+//   // Seq same
+//   if (JSON.stringify(tileSeq) === JSON.stringify(answerSeq)) {
+//     alert("WIn");
+//   }
+// }
 
-$(".tile").click(function () {
-  if (!isClickEnabled) return;
-  //   $(tileSeq[gameCount-1]).css("background-color", "#000");
+// $(".tile").click(function () {
+//   if (!isClickEnabled) return;
+//   //   $(tileSeq[gameCount-1]).css("background-color", "#000");
 
-  noOfClicks++;
-  if (noOfClicks <= gameCount) {
-    console.log("clicks = ", noOfClicks);
+//   noOfClicks++;
+//   if (noOfClicks <= gameCount) {
+//     console.log("clicks = ", noOfClicks);
 
-    tileIdString = $(this).attr("id"); // build the ID of which of the 16 elements (divs) was clicked
-    tileId = parseInt(tileIdString.split("tile")[1]);
-    console.log(tileId);
-    answerSeq.push(tileId);
+//     tileIdString = $(this).attr("id"); // build the ID of which of the 16 elements (divs) was clicked
+//     tileId = parseInt(tileIdString.split("tile")[1]);
+//     console.log(tileId);
+//     answerSeq.push(tileId);
 
-    // Glow the tile
-    var tileColor = getColour(tileId);
-    displayColouredTile(tileId, tileColor); // display users guess to screen grid
+//     // Glow the tile
+//     var tileColor = getColour(tileId);
+//     displayColouredTile(tileId, tileColor); // display users guess to screen grid
 
-    // (un)-glow the tile
-    var intervalID = setInterval(() => {
-      $("#tile" + tileId).css("background-color", "#000");
-      debugger;
-      clearTimeout(intervalID);
-    }, 1000);
-  }
-  console.log("NUMBER OF CLICKS = ", noOfClicks);
+//     // (un)-glow the tile
+//     var intervalID = setInterval(() => {
+//       $("#tile" + tileId).css("background-color", "#000");
+//       debugger;
+//       clearTimeout(intervalID);
+//     }, 1000);
+//   }
+//   console.log("NUMBER OF CLICKS = ", noOfClicks);
 
-  if (tileId !== tileSeq[index]) {
-    console.log(tileId, tileSeq[index]);
-    alert("! GAME OVER !");
-    debugger;
-  } else if (noOfClicks === gameCount) {
-    console.log("comparing noOfClicks to gameCount!!!");
-    //   $(".tile").off("click");
-    gameCount++; // increment game level (add 1 more tile)
-    noOfClicks = 0; // reset the clicks
+//   if (tileId !== tileSeq[index]) {
+//     console.log(tileId, tileSeq[index]);
+//     alert("! GAME OVER !");
+//     debugger;
+//   } else if (noOfClicks === gameCount) {
+//     console.log("comparing noOfClicks to gameCount!!!");
+//     //   $(".tile").off("click");
+//     gameCount++; // increment game level (add 1 more tile)
+//     noOfClicks = 0; // reset the clicks
 
-    answerSeq = []; // clear out the users' answer array
-    index = 0;
+//     answerSeq = []; // clear out the users' answer array
+//     index = 0;
 
-    tileId = pickTile(); // get a number between 1/16 inc
-    tileColor = getColour(tileId);
-    tileColorSeq.push(tileColor);
-    let x = 0;
-    displayCurrentSeq(tileSeq, x);
-  } else {
-    index++;
-  }
-});
+//     tileId = pickTile(); // get a number between 1/16 inc
+//     tileColor = getColour(tileId);
+//     tileColorSeq.push(tileColor);
+//     let x = 0;
+//     displayCurrentSeq(tileSeq, x);
+//   } else {
+//     index++;
+//   }
+// });
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,20 +1,35 @@
-console.log("OK");
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr1 = [];
 
-let randomArray = [];
-
-function printRandomArray() {
-
-  while (randomArray.length < 10) {
-    let num = Math.floor(Math.random() * 10) + 1;
-
-    if(!randomArray.includes(num)){
-        randomArray.push(num);
-    }
+function printArray() {
+  x = 0;
+  while (arr1.length < 10) {
+    let temp = arr[x];
+    arr1.push(temp);
+    x++;
   }
+  console.log(arr, arr1);
 }
 
-printRandomArray();
-console.log(randomArray);
+printArray();
+
+// console.log("OK");
+
+// let randomArray = [];
+
+// function printRandomArray() {
+
+//   while (randomArray.length < 10) {
+//     let num = Math.floor(Math.random() * 10) + 1;
+
+//     if(!randomArray.includes(num)){
+//         randomArray.push(num);
+//     }
+//   }
+// }
+
+// printRandomArray();
+// console.log(randomArray);
 
 // var arr1 = [];
 // var arr = [];
