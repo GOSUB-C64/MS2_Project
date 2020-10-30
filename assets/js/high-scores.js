d9a8a5c43f5f347921e1dfaaf83b4c404e5e75bd
Chris Curran <chriscurran8@yahoo.com>
2020-10-30 15:26:33 +0000

    working with the txt file to display default high scores

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,10 +1,18 @@
 //
 //
 $(document).ready(function () {
-  let scores = loadStrings('high-score-table.txt')
-  
-  let timeoutID = setTimeout(function () {
-    window.history.go(-1);
-    clearTimeout(timeoutID);
-  }, 5000);
+  fetch('assets/txt/high-score-table.txt')
+  .then(response => response.text())
+  .then(data => {
+      // Do something with the data
+      let myData = Array.from(data).join('');
+      let first = myData.split(' ');
+
+      console.log(first);
+      $('#highScores').text(first);
+  });
+//   let timeoutID = setTimeout(function () {
+//     window.history.go(-1);
+//     clearTimeout(timeoutID);
+//   }, 5000);
 });
